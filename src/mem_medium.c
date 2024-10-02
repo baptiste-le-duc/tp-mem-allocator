97a42914c413bddb35023ba76768ef5c0882ebec
Baptiste Le Duc <baptiste.le-duc@grenoble-inp.org>
2024-10-02 22:15:07 +0200

    Tests basic passed

--- src/mem_medium.c
+++ src/mem_medium.c
@@ -22,23 +22,23 @@ unsigned int puiss2(unsigned long size) {
 }
 
 void split(int n){
-    if (n == FIRST_ALLOC_MEDIUM_EXPOSANT + arena.medium_next_exponant){
-        mem_realloc_medium(n);
+    if (n >= FIRST_ALLOC_MEDIUM_EXPOSANT + arena.medium_next_exponant){
+        mem_realloc_medium();
     }
 
     if (arena.TZL[n] != NULL){
-        int child_block_size = n / 2;
-        void * head = arena.TZL[n];
-        void * buddy = (void *)(((intptr_t) head) ^ child_block_size) ; // Get buddy address
-        arena.TZL[n-1] = head; //Replace block in n-1 list
-        *((void **) head) = buddy; // ie next_addr de head = buddy
-        *(void **)buddy = NULL;
+        void * new_head = arena.TZL[n];
+        arena.TZL[n] = *((void**) new_head); //avance dans la liste
+        void *buddy = (void *)((intptr_t)new_head ^ (1 << (n-1)));
+        arena.TZL[n-1] = new_head; //Insert new head and its buddy to the linked list
+        *((void **) new_head) = buddy; // ie next_addr de head = buddy
+        *((void **) buddy) = NULL;
+
     }
     else {
         split(n+1);
     }
 }
-
 
 void * emalloc_medium(unsigned long size)
 {
@@ -47,18 +47,16 @@ void * emalloc_medium(unsigned long size)
 
     int idx = puiss2(size);
 
-    if (arena.TZL[idx]){ // Block found
-        void * head = arena.TZL[idx]; // Pointeur vers le premier élément de la liste 
-        void * next_addr = *((void**)head);
-        
-        void* user_ptr = mark_memarea_and_get_user_ptr(head, CHUNKSIZE, MEDIUM_KIND);
-        arena.TZL[idx] = next_addr;
-        return user_ptr;
+    while (1) { 
+        if (arena.TZL[idx]){ // Block found
+            void * head = arena.TZL[idx]; // Pointeur vers le premier élément de la liste 
+            void * next_addr = *((void**)head);
+            arena.TZL[idx] = next_addr;
+            void* user_ptr = mark_memarea_and_get_user_ptr(head, (1<< idx), MEDIUM_KIND);
+            return user_ptr;
+        }
+        split(idx + 1);
     }
-
-    split(idx + 1);
-    
-    return (void *) 0;
 }
 
 
